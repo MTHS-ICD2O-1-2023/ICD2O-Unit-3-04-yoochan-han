eb58ad39b95c4ea36612cf64bc19a14d904ef976
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-11 23:17:40 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+function CaseTest() {
+  const meter = parseFloat(document.getElementById("get_length").value)
+  const foot = meter * 3.281
+  document.getElementById("Ans").innerHTML = meter + "m" + " in feet is " + foot.toFixed(3) + "ft"
+}
