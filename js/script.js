ca1fd676cc53b28eb33324571d23e9ce44968fbe
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-06-03 20:24:55 +0000

    ##

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 // This file contains the JS functions for index.html
 
 function CaseTestFahrenheit() {
+  //get fahrenheit temperature 
   const fahrenheit = parseFloat(document.getElementById("get_fahrenheit").value)
+  //calculate the celsius 
   const celsius = (fahrenheit-32) * 5/9
   document.getElementById("Ans").innerHTML = fahrenheit + "ºF" + " in celsius is " + celsius.toFixed(3) + "ºC"
 }
